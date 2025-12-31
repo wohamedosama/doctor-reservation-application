615daa21763bbfb9d07debfba093e433edf20fb3
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-12-31 05:04:07 +0200

    - Configure the flutter flavor for android (Devlopment & Producation) - Added launch.json for android flavors config - added two main files for each flavor

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -4,8 +4,11 @@ import 'package:doctor_reservation_app/doc_app.dart';
 import 'package:doctor_reservation_app/my_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  // This is line written to handle the text's issue in the release mode while using screen_utls package
+  await ScreenUtil.ensureScreenSize();
   Bloc.observer = MyBlocObserver();
   setupGetIt();
   runApp(
